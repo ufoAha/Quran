09cdaf1667819d87056ef0ddf87168be5622a39f
Mousin Ahanger <mousin@192.168.1.12>
2026-05-03 15:39:27 +0530

    - Remove accidentally tracked java and class files - Fix launcher icon too small by using @mipmap launcher xml - Update build.sh accordingly and add the sign .aab bundle command

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 path=~/Projects/Quran
 version_code=7;version_name=7
+key_alias=uploadkey
 shopt -s extglob
 clean () {
     cd $path
@@ -25,31 +26,34 @@ clean
 
 cd $path
 
-aapt2 compile res/drawable/ic_launcher.png -o res/drawable/
 aapt2 compile res/values/styles.xml -o res/values/
+aapt2 compile res/drawable/ic_background.xml -o res/drawable
+aapt2 compile res/drawable/ic_foreground.xml -o res/drawable
+aapt2 compile res/mipmap-anydpi-v26/ic_launcher.xml -o res/mipmap-anydpi-v26/
 
 if [[ "$1" == "bundle" ]]; then
     dexer
     rm -r bundle && mkdir bundle
     aapt2 link --proto-format -o bundle/proto.zip -I ~/android_sdk/platforms/android-36/android.jar\
-    res/drawable/*flat res/values/*flat --manifest AndroidManifest.xml\
+    res/drawable/*flat res/values/*flat res/m*/*flat --manifest AndroidManifest.xml\
     --java src/ -A assets --version-code $version_code --version-name $version_name
     cd bundle && unzip proto.zip && rm proto.zip
     mkdir manifest && mkdir dex
     mv AndroidManifest.xml manifest && mv ../classes.dex dex
     zip -r module.zip * && rm -r !(*.zip)
     bundletool build-bundle --modules=module.zip --output=app.aab && rm module.zip
+    jarsigner -keystore ../my_keystore.p12 app.aab $key_alias
     exit
 else
     aapt2 link -o unsigned.apk -I ~/android_sdk/platforms/android-36/android.jar \
-    res/drawable/*flat res/values/*flat --manifest AndroidManifest.xml --java src/ -A assets
+    res/drawable/*flat res/values/*flat res/m*/*flat --manifest AndroidManifest.xml --java src/ -A assets
 fi
 
 dexer
 
 zip -u unsigned.apk classes.dex
 
-apksigner sign --ks my_keystore.jks --out signed.apk unsigned.apk <<< 1234312343
+apksigner sign --ks my_keystore.p12 --out signed.apk unsigned.apk <<< 1234312343
 
 adb uninstall truth.quran
 adb install signed.apk
