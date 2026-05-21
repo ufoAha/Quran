aa7b9222ab00c369d7c8250ead51bc549160c035
Mousin Ahanger <mousin@localhost.localdomain>
2026-05-21 10:13:51 +0530

    - From now on use js minify:uglifyjs npm

--- build.sh
+++ build.sh
@@ -12,11 +12,25 @@ clean () {
     rm res/values/*flat
     rm *dex
     rm unsigned.apk
+    rm -r compiled
 }
 
 function dexer () {
     javac src/QuranMain.java src/truth/quran/R.java -cp ~/android_sdk/platforms/android-36/android.jar
     SKIP_JDK_VERSION_CHECK=true d8 src/*class src/truth/quran/*class
+}
+
+function make_assets () {
+    mkdir -p compiled/assets
+    cp -r assets/css compiled/assets
+    cp -r assets/fonts compiled/assets
+    cp -r assets/surahs compiled/assets
+    cp assets/index.html compiled/assets
+
+    mkdir compiled/assets/js
+    uglifyjs assets/js/fuzzy_search.js -o compiled/assets/js/fuzzy_search.js
+    uglifyjs assets/js/script.js -o compiled/assets/js/script.js
+    
 }
 
 cd $path
@@ -27,9 +41,11 @@ aapt2 compile res/mipmap-anydpi-v26/ic_launcher.xml -o res/mipmap-anydpi-v26/
 
 if [[ "$1" == "bundle" ]]; then
     dexer
+    make_assets
     aapt2 link --proto-format -o bundle/proto.zip -I ~/android_sdk/platforms/android-36/android.jar\
     res/drawable/*flat res/values/*flat res/m*/*flat --manifest AndroidManifest.xml\
-    --java src/ -A assets --version-code $version_code --version-name $version_name
+    --java src/ -A compiled/assets --version-code $version_code --version-name $version_name
+    rm -r compiled
     cd bundle; unzip proto.zip; rm proto.zip
     if [[ -f app.aab ]]; then rm app.aab; fi
     mkdir manifest; mkdir dex
@@ -39,8 +55,10 @@ if [[ "$1" == "bundle" ]]; then
     jarsigner -keystore ../$keystore app.aab $key_alias <<< $pass
     exit
 else
+    make_assets
     aapt2 link -o unsigned.apk -I ~/android_sdk/platforms/android-36/android.jar \
-    res/drawable/*flat res/values/*flat res/m*/*flat --manifest AndroidManifest.xml --java src/ -A assets
+    res/drawable/*flat res/values/*flat res/m*/*flat --manifest AndroidManifest.xml --java src/ -A compiled/assets \
+    --version-code $version_code --version-name
 fi
 
 dexer
